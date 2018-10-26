235076fbe7a5ae18aadfd19381e8c651e0aef00a
Taylor Scott <t.skukx@gmail.com>
2018-10-25 21:49:44 -0700

    Add token distribution for devise.
    Right now only provide a distributor for devise. This module can be
    included into the application controller of an app which will active the
    token distribution.
    
    The workflow is when a user signs in, they will be redirected back with
    the header `X-SPREE-TOKEN`.

---
+++ lib/solidus_jwt/distributor/devise.rb
@@ -0,0 +1,15 @@
+module SolidusJwt
+  module Distributor
+    module Devise
+      def after_sign_in_path_for(resource)
+        # Send back json web token in redirect header
+        if try_spree_current_user
+          response.headers['X-SPREE-TOKEN'] = try_spree_current_user.
+            generate_jwt_token(expires_in: SolidusJwt::Config.jwt_expiration)
+        end
+
+        super
+      end
+    end
+  end
+end

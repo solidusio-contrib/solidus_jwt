dc13924ae9cdd6dda7f20deb84b1468715cec76a
Taylor Scott <t.skukx@gmail.com>
2019-11-01 13:32:00 -0700

    version bump

--- lib/solidus_jwt/version.rb
+++ lib/solidus_jwt/version.rb
@@ -1,8 +1,8 @@
 module SolidusJwt
-  MAJOR = 0
-  MINOR = 1
+  MAJOR = 1
+  MINOR = 0
   PATCH = 0
-  PRERELEASE = nil
+  PRERELEASE = 'beta1'
 
   def self.version
     version = [MAJOR, MINOR, PATCH].join('.')

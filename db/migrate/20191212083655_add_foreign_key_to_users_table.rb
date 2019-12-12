bb32535e1b73e803e2a56089df7e1c980be988b6
Thomas von Deyen <thomas@vondeyen.com>
2019-12-12 09:42:53 +0100

    Add foreign key to users table
    Having a foreign key between users table and the tokens table
    is not only helping to maintain structural integrity in the database
    it also mitigates security issues by making sure that the token
    gets removed if the user gets deleted.

---
+++ db/migrate/20191212083655_add_foreign_key_to_users_table.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key :solidus_jwt_tokens, Spree.user_class.table_name, column: :user_id, on_delete: :cascade
+  end
+end
